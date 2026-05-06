d1cb5f8c5c1df9073089fa9f383483430b15cb83
Mab <mabthemage@gmail.com>
2026-05-05 21:06:35 -0400

    finished chapter

--- kilo.c
+++ kilo.c
@@ -16,6 +16,18 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f) //macro to bitwise and the key entered with 00011111 which will take the 5 and 6 bits off (bits start at 0)this would change a (01100001) to 00000001 and A (01000001) to 00000001
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
+};
+
 /*** data ***/
 
 struct editorConfig {
@@ -58,13 +70,54 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-char editorReadKey() {//reads key and returns it c
+int editorReadKey() {//reads key and returns it c
 	int nread;
 	char c;
 	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
 		if (nread == -1 && errno != EAGAIN) die("read");
 	}
-	return c;
+
+	if (c == '\x1b') {
+		char seq[3];
+
+		if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+		if (seq[0] == '[') {
+			if (seq[1] >= '0' && seq[1] <= '9') {
+				if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+				if (seq[2] == '~') {
+					switch (seq[1]) {
+						case '1': return HOME_KEY;
+						case '2': return DEL_KEY;
+						case '4': return END_KEY;
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+						case '7': return HOME_KEY;
+						case '8': return END_KEY;
+					}
+				}
+			} else {
+				switch (seq[1]) {
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+					case 'F': return END_KEY;
+				}
+			}
+		} else if (seq[0] == 'O') {
+			switch (seq[1]) {
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
+			}
+		}
+
+		return '\x1b';
+	} else {
+		return c;
+	}
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -123,14 +176,63 @@ void abFree(struct abuf *ab) {
 
 /*** input ***/
 
+void editorMoveCursor(int key) {
+	switch (key){
+		case ARROW_LEFT:
+			if (E.cx != 0) {
+				E.cx--;
+			}
+			break;
+		case ARROW_RIGHT:
+			if (E.cx != E.screencols - 1) {
+				E.cx++;
+			}
+			break;
+		case ARROW_UP:
+			if (E.cy != 0) {
+				E.cy--;
+			}
+			break;
+		case ARROW_DOWN:
+			if (E.cy != E.screenrows - 1) {
+				E.cy++;
+			}
+			break;
+	}
+}
+
 void editorProcessKeypress() {//checks for ctrl+q
-	char c = editorReadKey();
+	int c = editorReadKey();
 
 	switch (c) {
 		case CTRL_KEY('q'):
 			write(STDOUT_FILENO, "\x1b[2J", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+
+		case END_KEY:
+			E.cx = E.screencols - 1;
+			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+				int times = E.screenrows;
+				while (times--)
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP: ARROW_DOWN);
+			}
+			break;
+
+		case ARROW_UP:
+		case ARROW_DOWN:
+		case ARROW_LEFT:
+		case ARROW_RIGHT:
+			editorMoveCursor(c);
 			break;
 	}
 }
